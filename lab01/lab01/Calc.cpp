62c247a30fba00cef33d59a439e6e9594803114b
Unknown <Pasha-Love-comp@mail.ru>
2018-09-28 23:48:33 +0500

    Visual Hotfix for cout

--- lab01/lab01/Calc.cpp
+++ lab01/lab01/Calc.cpp
@@ -13,22 +13,36 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 
-	cout << "Какое действие вы будете выполнять?" << endl << "1. +" << endl << "2. -" << endl << "3. *" << endl << "4. /" << endl << "5. a в степени b" << endl << "6. b корней из a" << endl << "7. Loga(b)" << endl << "Ваш ответ: ";
+	cout << "Какое действие вы будете выполнять?" << endl 
+		<< "1. +" << endl << "2. -" << endl 
+		<< "3. *" << endl << "4. /" << endl 
+		<< "5. a в степени b" << endl 
+		<< "6. b корней из a" << endl 
+		<< "7. Loga(b)" << endl 
+		<< "Ваш ответ: ";
+
 	while (!(cin >> act) || (cin.get() != '\n') || (act < 1) || (act > 7)) {
 		cin.clear();
-		cout << "Вам необходимо ввести число от 1 до 7." << endl << endl;
+		cout << "Вам необходимо ввести число от 1 до 7." << endl 
+			<< endl;
 	}
 
-	cout << endl << "Введите числа." << endl << "Число a: ";
+	cout << endl 
+		<< "Введите числа." << endl 
+		<< "Число a: ";
+
 	while (!(cin >> a) || (cin.get() != '\n')) {
 		cin.clear();
-		cout << "Необходимо ввести число." << endl << "Число a: ";
+		cout << "Необходимо ввести число." << endl 
+			<< "Число a: ";
 	}
 
 	cout << "Число b: ";
+
 	while (!(cin >> b) || (cin.get() != '\n')) {
 		cin.clear();
-		cout << "Необходимо ввести число." << endl << "Число b: ";
+		cout << "Необходимо ввести число." << endl 
+			<< "Число b: ";
 	}
 
 	Calc();
@@ -63,18 +77,24 @@ void Calc()
 		result = log(a) / log(b);
 	}
 
-	cout << "Результат = " << result << endl << endl;
+	cout << "Результат = " << result << endl 
+		<< endl;
 
 	CalcEnd();
 }
 
 void CalcEnd()
 {
-	cout << "Хотите ли вы повторить?" << endl << "1. Да" << endl << "2. Нет" << endl << endl;
+	cout << "Хотите ли вы повторить?" << endl 
+		<< "1. Да" << endl << "2. Нет" << endl 
+		<< endl;
+
 	int repeat = 0;
+
 	while (!(cin >> repeat) || (cin.get() != '\n') || (repeat < 1) || (repeat > 2)); {
 		cin.clear();
-		cout << "Вам необходимо ввести '1' или '2'." << endl << endl;
+		cout << "Вам необходимо ввести '1' или '2'." << endl 
+			<< endl;
 	}
 
 	switch (repeat) {
